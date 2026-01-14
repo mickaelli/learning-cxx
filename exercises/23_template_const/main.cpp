5535cf858c05a1ffaa7385f3e4392c18cbf5349c
Hollonemo <mickael.li@outlook.com>
2026-01-14 15:47:29 +0800

    Completed exercises 0-33

--- exercises/23_template_const/main.cpp
+++ exercises/23_template_const/main.cpp
@@ -11,6 +11,10 @@ struct Tensor {
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for(int i=0;i<4;++i){
+            shape[i]=shape_[i];
+            size*=shape[i];
+        }
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
@@ -34,6 +38,7 @@ private:
         unsigned int index = 0;
         for (unsigned int i = 0; i < N; ++i) {
             ASSERT(indices[i] < shape[i], "Invalid index");
+            index = index * shape[i] + indices[i];
             // TODO: 计算 index
         }
         return index;
